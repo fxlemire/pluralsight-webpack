ae6a898d0fb9719c47e7f1dea1c4d46cfc3f8260
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2016-04-23 14:17:04 -0400

    webpack react build

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-/* eslint-disable no-console */
-import {} from './login';
+/* eslint-disable func-names,no-console,no-magic-numbers,object-shorthand,space-before-function-paren */
+import React from 'react';
+import ReactDom from 'react-dom';
 
-console.log('App loaded!');
-$('#testDiv').text('jQuery modified this content (see app.js)');
+const Timer = React.createClass({
+  getInitialState() {
+    return {secondsElapsed: 0};
+  },
+
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillMount: function() {
+    clearInterval(this.interval);
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h3>It's the final countdown</h3>
+        Seconds Elapsed: {this.state.secondsElapsed}
+      </div>
+    );
+  }
+});
+
+ReactDom.render(<Timer />, document.getElementById('container'));
+
+require('./login');
